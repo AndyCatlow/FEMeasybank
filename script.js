0cfc550eaa88f6e625ce5ed58710f47cbbd14bdb
AndyCatlow <mrcatlow@gmail.com>
2022-07-05 17:17:34 +0200

    move articles to json file

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const toggleNavBtn = document.querySelector(".nav-toggle-btn");
 const menu = document.querySelector(".nav-list");
+const articlesContainer = document.querySelector("#article--cards__container");
 
 toggleNavBtn.addEventListener("click", toggleNav);
 
@@ -11,3 +12,38 @@ function toggleNav() {
     ? "./images/icon-close.svg"
     : "./images/icon-hamburger.svg");
 }
+
+function getArticlesData() {
+  fetch("articles.json")
+    .then((response) => response.json())
+    .then((data) => {
+      getArticlesHtml(data);
+    });
+}
+
+function getArticlesHtml(arr) {
+  let articlesHtml = "";
+  arr.forEach((article) => {
+    articlesHtml += `
+    <div class="article--card">
+    <img
+      src="${article.image}"
+      alt="${article.altText}"
+      class="article--card__img"
+    />
+    <div class="article--card--text__container">
+      <p class="article--card__author">By ${article.author}</p>
+      <h3 class="article--card__title">
+      ${article.title}
+      </h3>
+      <p class="article--card__text">
+      ${article.text}
+      </p>
+    </div>
+  </div>
+</div>
+    `;
+  });
+  articlesContainer.innerHTML = articlesHtml;
+}
+getArticlesData();
